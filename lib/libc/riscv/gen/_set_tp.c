a98e73f32959a79ba7621a8591963e9306580397
Ruslan Bukin <br@bsdpad.com>
2015-10-02 15:04:16 +0000

    set tp correctly

--- lib/libc/riscv/gen/_set_tp.c
+++ lib/libc/riscv/gen/_set_tp.c
@@ -37,7 +37,10 @@ __FBSDID("$FreeBSD$");
 void
 _set_tp(void *tp)
 {
+	uint64_t p;
+
+	p = *(uint64_t *)tp;
 
 	//asm volatile("msr	tpidr_el0, %0" : : "r"(tp));
-	__asm __volatile("mv tp, %0" :: "r"(tp));
+	__asm __volatile("mv	x4, %0" :: "r"(p));
 }
