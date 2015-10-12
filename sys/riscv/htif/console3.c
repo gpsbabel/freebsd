a509de85efe8ea06a1fd2a0b3b47941f973d5d43
Ruslan Bukin <br@bsdpad.com>
2015-10-12 13:22:43 +0000

    Add template console driver

---
+++ sys/riscv/htif/console3.c
@@ -0,0 +1,250 @@
+/*-
+ * Copyright (C) 2008 by Nathan Whitehorn. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TOOLS GMBH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/kdb.h>
+#include <sys/kernel.h>
+#include <sys/priv.h>
+#include <sys/systm.h>
+#include <sys/types.h>
+#include <sys/conf.h>
+#include <sys/cons.h>
+#include <sys/consio.h>
+#include <sys/tty.h>
+#include <machine/htif.h>
+
+#include <dev/ofw/openfirm.h>
+
+#include <ddb/ddb.h>
+
+//#include "riscvcall.h"
+
+#define MAMBOBURSTLEN	4096	/* max number of bytes to write in one chunk */
+
+#define MAMBO_CONSOLE_WRITE	0
+#define MAMBO_CONSOLE_READ	60
+
+extern uint64_t console_data;
+
+static tsw_outwakeup_t riscvtty_outwakeup;
+
+static struct ttydevsw riscv_ttydevsw = {
+	.tsw_flags	= TF_NOPREFIX,
+	.tsw_outwakeup	= riscvtty_outwakeup,
+};
+
+static int			polltime;
+static struct callout		riscv_callout;
+static struct tty 		*tp = NULL;
+
+#if defined(KDB)
+static int			alt_break_state;
+#endif
+
+static void	riscv_timeout(void *);
+
+static cn_probe_t	riscv_cnprobe;
+static cn_init_t	riscv_cninit;
+static cn_term_t	riscv_cnterm;
+static cn_getc_t	riscv_cngetc;
+static cn_putc_t	riscv_cnputc;
+static cn_grab_t	riscv_cngrab;
+static cn_ungrab_t	riscv_cnungrab;
+
+CONSOLE_DRIVER(riscv);
+
+static uint64_t
+htif_cmd(uint64_t cmd)
+{
+	uint64_t res;
+
+	__asm __volatile(
+		"mv	t5, %2\n"
+		"mv	t6, %1\n"
+		"ecall\n"
+		"mv	%0, t6" : "=&r"(res) : "r"(cmd), "r"(ECALL_LOW_PRINTC)
+	);
+
+	return (res);
+}
+
+static void 
+htif_early_putc(int c)
+{
+	uint64_t cmd;
+
+	cmd = 0x101000000000000;
+	cmd |= c;
+
+	htif_cmd(cmd);
+}
+
+static uint8_t
+htif_getc(void)
+{
+	uint64_t cmd;
+	uint8_t res;
+
+	cmd = 0x100000000000000;
+
+	res = htif_cmd(cmd);
+
+	return (res);
+}
+
+static void
+cn_drvinit(void *unused)
+{
+
+	if (riscv_consdev.cn_pri != CN_DEAD &&
+	    riscv_consdev.cn_name[0] != '\0') {
+		//if (OF_finddevice("/riscv") == -1)
+		//	return;
+
+		tp = tty_alloc(&riscv_ttydevsw, NULL);
+		tty_init_console(tp, 0);
+		tty_makedev(tp, NULL, "%s", "riscvcons");
+
+		polltime = 1;
+
+		callout_init(&riscv_callout, 1);
+		callout_reset(&riscv_callout, polltime, riscv_timeout, NULL);
+	}
+}
+
+SYSINIT(cndev, SI_SUB_CONFIGURE, SI_ORDER_MIDDLE, cn_drvinit, NULL);
+
+static void
+riscvtty_outwakeup(struct tty *tp)
+{
+	int len;
+	int i;
+	u_char buf[MAMBOBURSTLEN];
+
+	for (;;) {
+		len = ttydisc_getc(tp, buf, sizeof buf);
+		if (len == 0)
+			break;
+		//riscvcall(MAMBO_CONSOLE_WRITE, buf, (register_t)len, 1UL);
+		for (i = 0; i < len; i++)
+			htif_early_putc(buf[i]);
+	}
+}
+
+static void
+riscv_timeout(void *v)
+{
+	int 	c;
+
+	tty_lock(tp);
+	while ((c = riscv_cngetc(NULL)) != -1)
+		ttydisc_rint(tp, c, 0);
+	ttydisc_rint_done(tp);
+	tty_unlock(tp);
+
+	callout_reset(&riscv_callout, polltime, riscv_timeout, NULL);
+}
+
+void
+riscv_console_intr(void)
+{
+	uint64_t *cc = &console_data;
+	int c = *(uint8_t *)cc & 0xff;
+
+	//tty_lock(tp);
+	if (c) {
+		ttydisc_rint(tp, c, 0);
+	}
+	ttydisc_rint_done(tp);
+	//tty_unlock(tp);
+}
+
+static void
+riscv_cnprobe(struct consdev *cp)
+{
+	//if (OF_finddevice("/riscv") == -1) {
+	//	cp->cn_pri = CN_DEAD;
+	//	return;
+	//}
+
+	cp->cn_pri = CN_NORMAL;
+}
+
+static void
+riscv_cninit(struct consdev *cp)
+{
+
+	/* XXX: This is the alias, but that should be good enough */
+	strcpy(cp->cn_name, "riscvcons");
+}
+
+static void
+riscv_cnterm(struct consdev *cp)
+{
+}
+
+static void
+riscv_cngrab(struct consdev *cp)
+{
+}
+
+static void
+riscv_cnungrab(struct consdev *cp)
+{
+}
+
+static int
+riscv_cngetc(struct consdev *cp)
+{
+	int ch;
+
+	//uint64_t *cc = &console_data;
+	//uint8_t c = *(uint8_t *)cc;
+
+	//ch = 0; //riscvcall(MAMBO_CONSOLE_READ);
+	ch = htif_getc();
+	//ch = c;
+	if (ch > 0 && ch < 0xff) {
+		//*cc = 0;
+#if defined(KDB)
+		kdb_alt_break(ch, &alt_break_state);
+#endif
+		//return (ch);
+	}
+
+	return (-1);
+}
+
+static void
+riscv_cnputc(struct consdev *cp, int c)
+{
+	char cbuf;
+
+	cbuf = c;
+	//riscvcall(MAMBO_CONSOLE_WRITE, &cbuf, 1UL, 1UL);
+	htif_early_putc(c);
+}
