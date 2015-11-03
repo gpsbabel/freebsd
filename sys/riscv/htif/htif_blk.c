5fa5da008a761e074f0f6b70b2453565bc8c31cf
Ruslan Bukin <br@bsdpad.com>
2015-11-03 18:51:53 +0000

    WIP: add template for block device

---
+++ sys/riscv/htif/htif_blk.c
@@ -0,0 +1,481 @@
+/*-
+ * Copyright (c) 2006 IronPort Systems
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/selinfo.h>
+#include <sys/module.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/malloc.h>
+#include <sys/sysctl.h>
+#include <sys/uio.h>
+
+#include <sys/bio.h>
+#include <sys/bus.h>
+#include <sys/conf.h>
+#include <sys/disk.h>
+#include <geom/geom_disk.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+
+#include <machine/md_var.h>
+#include <machine/bus.h>
+#include <machine/htif.h>
+#include <machine/trap.h>
+#include <sys/rman.h>
+
+//#include <dev/htif/htifreg.h>
+//#include <dev/htif/htif_ioctl.h>
+//#include <dev/htif/htifvar.h>
+#include "htif.h"
+#include "htif_blk.h"
+
+static int	htif_blk_probe(device_t dev);
+static int	htif_blk_attach(device_t dev);
+static int	htif_blk_detach(device_t dev);
+
+static disk_open_t	htif_blk_open;
+static disk_close_t	htif_blk_close;
+static disk_strategy_t	htif_blk_strategy;
+static dumper_t		htif_blk_dump;
+
+static devclass_t	htif_blk_devclass;
+
+static device_method_t htif_blk_methods[] = {
+	DEVMETHOD(device_probe,		htif_blk_probe),
+	DEVMETHOD(device_attach,	htif_blk_attach),
+	DEVMETHOD(device_detach,	htif_blk_detach),
+	{ 0, 0 }
+};
+
+struct htif_blk_softc {
+	device_t	dev;
+	struct disk	*disk;
+	struct htif_dev_softc *sc_dev;
+	struct mtx	htif_io_mtx;
+};
+
+static driver_t htif_blk_driver = {
+	"htif_blk",
+	htif_blk_methods,
+	sizeof(struct htif_blk_softc)
+};
+
+DRIVER_MODULE(htif_blk, htif, htif_blk_driver, htif_blk_devclass, 0, 0);
+
+static int
+htif_blk_probe(device_t dev)
+{
+
+	printf("htif blk probe\n");
+	return (0);
+}
+
+static int
+htif_blk_attach(device_t dev)
+{
+	struct htif_blk_softc *sc;
+	struct htif_dev_softc *sc_dev;
+	long size;
+
+	sc = device_get_softc(dev);
+	sc_dev = device_get_ivars(dev);
+	sc->sc_dev = sc_dev;
+	mtx_init(&sc->htif_io_mtx, device_get_nameunit(dev), "htif_blk", MTX_DEF);
+
+	char *str;
+	char prefix[] = " size=";
+
+	str = strstr(sc_dev->id, prefix);
+
+	size = strtol((str + 6), NULL, 10);
+	if (size == 0) {
+		return (ENXIO);
+	}
+
+	printf("htif blk attach, size %ld\n", size);
+
+	printf("disabled\n");
+	return (0);
+
+#if 0
+	struct htif_ld_info *ld_info;
+	struct htif_blk_pending *ld_pend;
+	uint64_t sectors;
+	uint32_t secsize;
+	char *state;
+
+	ld_info = device_get_ivars(dev);
+
+	sc->ld_dev = dev;
+	sc->ld_id = ld_info->ld_config.properties.ld.v.target_id;
+	sc->ld_unit = device_get_unit(dev);
+	sc->ld_info = ld_info;
+	sc->ld_controller = device_get_softc(device_get_parent(dev));
+	sc->ld_flags = 0;
+
+	sectors = ld_info->size;
+	secsize = MFI_SECTOR_LEN;
+	mtx_lock(&sc->ld_controller->htif_io_mtx);
+	TAILQ_INSERT_TAIL(&sc->ld_controller->htif_ld_tqh, sc, ld_link);
+	TAILQ_FOREACH(ld_pend, &sc->ld_controller->htif_ld_pend_tqh,
+	    ld_link) {
+		TAILQ_REMOVE(&sc->ld_controller->htif_ld_pend_tqh,
+		    ld_pend, ld_link);
+		free(ld_pend, M_MFIBUF);
+		break;
+	}
+	mtx_unlock(&sc->ld_controller->htif_io_mtx);
+
+	switch (ld_info->ld_config.params.state) {
+	case MFI_LD_STATE_OFFLINE:
+		state = "offline";
+		break;
+	case MFI_LD_STATE_PARTIALLY_DEGRADED:
+		state = "partially degraded";
+		break;
+	case MFI_LD_STATE_DEGRADED:
+		state = "degraded";
+		break;
+	case MFI_LD_STATE_OPTIMAL:
+		state = "optimal";
+		break;
+	default:
+		state = "unknown";
+		break;
+	}
+
+	if ( strlen(ld_info->ld_config.properties.name) == 0 ) {
+		device_printf(dev,
+		      "%juMB (%ju sectors) RAID volume (no label) is %s\n",
+		       sectors / (1024 * 1024 / secsize), sectors, state);
+	} else {
+		device_printf(dev,
+		      "%juMB (%ju sectors) RAID volume '%s' is %s\n",
+		      sectors / (1024 * 1024 / secsize), sectors,
+		      ld_info->ld_config.properties.name, state);
+	}
+
+	sc->ld_disk = disk_alloc();
+	sc->ld_disk->d_drv1 = sc;
+	sc->ld_disk->d_maxsize = min(sc->ld_controller->htif_max_io * secsize,
+	    (sc->ld_controller->htif_max_sge - 1) * PAGE_SIZE);
+	sc->ld_disk->d_name = "htifd";
+	sc->ld_disk->d_open = htif_blk_open;
+	sc->ld_disk->d_close = htif_blk_close;
+	sc->ld_disk->d_strategy = htif_blk_strategy;
+	sc->ld_disk->d_dump = htif_blk_dump;
+	sc->ld_disk->d_unit = sc->ld_unit;
+	sc->ld_disk->d_sectorsize = secsize;
+	sc->ld_disk->d_mediasize = sectors * secsize;
+	if (sc->ld_disk->d_mediasize >= (1 * 1024 * 1024)) {
+		sc->ld_disk->d_fwheads = 255;
+		sc->ld_disk->d_fwsectors = 63;
+	} else {
+		sc->ld_disk->d_fwheads = 64;
+		sc->ld_disk->d_fwsectors = 32;
+	}
+	sc->ld_disk->d_flags = DISKFLAG_UNMAPPED_BIO;
+	disk_create(sc->ld_disk, DISK_VERSION);
+#endif
+
+	sc->disk = disk_alloc();
+	sc->disk->d_drv1 = sc;
+
+	//sc->ld_disk->d_maxsize = min(sc->ld_controller->htif_max_io * secsize,
+	//    (sc->ld_controller->htif_max_sge - 1) * PAGE_SIZE);
+
+	sc->disk->d_maxsize = 4096;
+	sc->disk->d_name = "htif_blk";
+	sc->disk->d_open = htif_blk_open;
+	sc->disk->d_close = htif_blk_close;
+	sc->disk->d_strategy = htif_blk_strategy;
+	sc->disk->d_dump = htif_blk_dump;
+	sc->disk->d_unit = 0;
+	sc->disk->d_sectorsize = SECTOR_SIZE;
+	sc->disk->d_mediasize = size;
+	disk_create(sc->disk, DISK_VERSION);
+
+#if 0
+	sc->disk->d_unit = sc->ld_unit;
+	sc->disk->d_sectorsize = secsize;
+	sc->disk->d_mediasize = sectors * secsize;
+	if (sc->ld_disk->d_mediasize >= (1 * 1024 * 1024)) {
+		sc->ld_disk->d_fwheads = 255;
+		sc->ld_disk->d_fwsectors = 63;
+	} else {
+		sc->ld_disk->d_fwheads = 64;
+		sc->ld_disk->d_fwsectors = 32;
+	}
+	sc->ld_disk->d_flags = DISKFLAG_UNMAPPED_BIO;
+
+	disk_create(sc->ld_disk, DISK_VERSION);
+#endif
+
+	return (0);
+}
+
+static int
+htif_blk_detach(device_t dev)
+{
+
+	printf("%s\n", __func__);
+
+#if 0
+	struct htif_blk *sc;
+
+	sc = device_get_softc(dev);
+
+	mtx_lock(&sc->ld_controller->htif_io_lock);
+	if (((sc->ld_disk->d_flags & DISKFLAG_OPEN) ||
+	    (sc->ld_flags & MFI_DISK_FLAGS_OPEN)) &&
+	    (sc->ld_controller->htif_keep_deleted_volumes ||
+	    sc->ld_controller->htif_detaching)) {
+		mtx_unlock(&sc->ld_controller->htif_io_lock);
+		return (EBUSY);
+	}
+	mtx_unlock(&sc->ld_controller->htif_io_lock);
+
+	disk_destroy(sc->ld_disk);
+	mtx_lock(&sc->ld_controller->htif_io_lock);
+	TAILQ_REMOVE(&sc->ld_controller->htif_ld_tqh, sc, ld_link);
+	mtx_unlock(&sc->ld_controller->htif_io_lock);
+	free(sc->ld_info, M_MFIBUF);
+#endif
+	return (0);
+}
+
+static int
+htif_blk_open(struct disk *dp)
+{
+	int error;
+
+	printf("%s\n", __func__);
+
+	error = 0;
+#if 0
+	struct htif_blk *sc;
+
+	sc = dp->d_drv1;
+	mtx_lock(&sc->ld_controller->htif_io_lock);
+	if (sc->ld_flags & MFI_DISK_FLAGS_DISABLED)
+		error = ENXIO;
+	else {
+		sc->ld_flags |= MFI_DISK_FLAGS_OPEN;
+		error = 0;
+	}
+	mtx_unlock(&sc->ld_controller->htif_io_lock);
+#endif
+
+	return (error);
+}
+
+static int
+htif_blk_close(struct disk *dp)
+{
+#if 0
+	struct htif_blk *sc;
+
+	sc = dp->d_drv1;
+	mtx_lock(&sc->ld_controller->htif_io_lock);
+	sc->ld_flags &= ~MFI_DISK_FLAGS_OPEN;
+	mtx_unlock(&sc->ld_controller->htif_io_lock);
+
+#endif
+	return (0);
+}
+
+#if 0
+int
+htif_blk_disable(struct htif_blk_softc *sc)
+{
+	mtx_assert(&sc->ld_controller->htif_io_lock, MA_OWNED);
+	if (sc->ld_flags & MFI_DISK_FLAGS_OPEN) {
+		if (sc->ld_controller->htif_delete_busy_volumes)
+			return (0);
+		device_printf(sc->ld_dev, "Unable to delete busy ld device\n");
+		return (EBUSY);
+	}
+	sc->ld_flags |= MFI_DISK_FLAGS_DISABLED;
+	return (0);
+}
+#endif
+
+#if 0
+void
+htif_blk_enable(struct htif_blk_softc *sc)
+{
+
+	mtx_assert(&sc->ld_controller->htif_io_lock, MA_OWNED);
+	sc->ld_flags &= ~MFI_DISK_FLAGS_DISABLED;
+}
+#endif
+
+static void
+htif_blk_strategy(struct bio *bp)
+{
+	struct htif_blk_softc *sc;
+	struct htif_dev_softc *sc_dev;
+	int block;
+	int bcount;
+	uint64_t cmd;
+	uint64_t paddr;
+
+	struct htif_blk_request req __aligned(HTIF_ALIGN);
+
+	sc = bp->bio_disk->d_drv1;
+	sc_dev = sc->sc_dev;
+
+	//printf("%s\n", __func__);
+
+	mtx_lock(&sc->htif_io_mtx);
+
+	if (bp->bio_cmd == BIO_READ || bp->bio_cmd == BIO_WRITE) {
+		block = bp->bio_pblkno;
+		bcount = bp->bio_bcount;
+		printf("%d block %d count %d\n", bp->bio_cmd, block, bcount);
+
+		req.offset = (bp->bio_pblkno * sc->disk->d_sectorsize);
+		req.size = bp->bio_bcount;
+		paddr = vtophys(bp->bio_data);
+		KASSERT(paddr != 0, ("paddr is 0"));
+		req.addr = paddr;
+		req.tag = 0;
+
+		//printf("index %d addr 0x%016lx\n", sc_dev->index, req.addr);
+		cmd = sc_dev->index;
+		cmd <<= 56;
+		if (bp->bio_cmd == BIO_READ)
+			cmd |= (HTIF_CMD_READ << 48);
+		else
+			cmd |= (HTIF_CMD_WRITE << 48);
+		paddr = vtophys(&req);
+		KASSERT(paddr != 0, ("paddr is 0"));
+		cmd |= paddr;
+		htif_command(cmd, ECALL_HTIF_CMD);
+		biodone(bp);
+		//biofinish(bp, NULL, ENXIO);
+	} else {
+		printf("unknown op %d\n", bp->bio_cmd);
+	}
+
+	mtx_unlock(&sc->htif_io_mtx);
+
+	//printf("%s done\n", __func__);
+#if 0
+	struct htif_blk *sc;
+	struct htif_softc *controller;
+
+	sc = bio->bio_disk->d_drv1;
+
+	if (sc == NULL) {
+		bio->bio_error = EINVAL;
+		bio->bio_flags |= BIO_ERROR;
+		bio->bio_resid = bio->bio_bcount;
+		biodone(bio);
+		return;
+	}
+
+	controller = sc->ld_controller;
+	if (controller->adpreset) {
+		bio->bio_error = EBUSY;
+		return;
+	}
+
+	if (controller->hw_crit_error) {
+		bio->bio_error = EBUSY;
+		return;
+	}
+
+	if (controller->issuepend_done == 0) {
+		bio->bio_error = EBUSY;
+		return;
+	}
+
+	bio->bio_driver1 = (void *)(uintptr_t)sc->ld_id;
+	/* Mark it as LD IO */
+	bio->bio_driver2 = (void *)MFI_LD_IO;
+	mtx_lock(&controller->htif_io_lock);
+	htif_enqueue_bio(controller, bio);
+	htif_startio(controller);
+	mtx_unlock(&controller->htif_io_lock);
+#endif
+	return;
+}
+
+#if 0
+void
+htif_blk_complete(struct bio *bio)
+{
+	struct htif_blk_softc *sc;
+	struct htif_frame_header *hdr;
+
+	sc = bio->bio_disk->d_drv1;
+	hdr = bio->bio_driver1;
+
+	if (bio->bio_flags & BIO_ERROR) {
+		bio->bio_resid = bio->bio_bcount;
+		if (bio->bio_error == 0)
+			bio->bio_error = EIO;
+		disk_err(bio, "hard error", -1, 1);
+	} else {
+		bio->bio_resid = 0;
+	}
+	biodone(bio);
+}
+#endif
+
+static int
+htif_blk_dump(void *arg, void *virt, vm_offset_t phys, off_t offset, size_t len)
+{
+	printf("%s\n", __func__);
+#if 0
+	struct htif_blk_softc *sc;
+	struct htif_softc *parent_sc;
+	struct disk *dp;
+	int error;
+
+	dp = arg;
+	sc = dp->d_drv1;
+	parent_sc = sc->ld_controller;
+
+	if (len > 0) {
+		if ((error = htif_dump_blocks(parent_sc, sc->ld_id, offset /
+		    MFI_SECTOR_LEN, virt, len)) != 0)
+			return (error);
+	} else {
+		/* htif_sync_cache(parent_sc, sc->ld_id); */
+	}
+#endif
+
+	return (0);
+}
---
+++ sys/riscv/htif/htif_blk.h
@@ -0,0 +1,9 @@
+struct htif_blk_request {
+	uint64_t addr;
+	uint64_t offset;	/* offset in bytes */
+	uint64_t size;		/* length in bytes */
+	uint64_t tag;
+} __packed;
+
+#define	SECTOR_SIZE_SHIFT	9
+#define	SECTOR_SIZE		(1 << SECTOR_SIZE_SHIFT)
