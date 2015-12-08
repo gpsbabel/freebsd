49cc423e024d306b045de4ea0f4ebca47c916098
Ruslan Bukin <br@bsdpad.com>
2015-12-08 13:40:31 +0000

    Cleanup

--- sys/riscv/riscv/ric.c
+++ sys/riscv/riscv/ric.c
@@ -33,7 +33,7 @@
  */
 
 /*
- * RISC-V Interrupt Controller
+ * RISC-V Interrupt Controller (RIC)
  */
 
 #include <sys/cdefs.h>
@@ -62,9 +62,11 @@ __FBSDID("$FreeBSD$");
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
 
+#include "pic_if.h"
+
 struct riscv_ric_softc {
-	device_t		dev;
-	uint32_t		nirqs;
+	device_t dev;
+	uint32_t nirqs;
 };
 
 static struct ofw_compat_data compat_data[] = {
@@ -72,23 +74,15 @@ static struct ofw_compat_data compat_data[] = {
 	{NULL,			false}
 };
 
-#include "pic_if.h"
-
-//#define	NR_IRQS		2
-//#define	IRQ_SOFTWARE	0
-//#define	IRQ_TIMER	1
-
 enum {
 	IRQ_SOFTWARE,
 	IRQ_TIMER,
 	NIRQS
 };
-#define	SR_IE		(1 << 0)
 
 static struct riscv_ric_softc *riscv_ric_sc = NULL;
 
 static pic_dispatch_t ric_dispatch;
-static pic_eoi_t ric_eoi;
 static pic_mask_t ric_mask_irq;
 static pic_unmask_t ric_unmask_irq;
 
@@ -119,11 +113,8 @@ riscv_ric_attach(device_t dev)
 
 	riscv_ric_sc = sc;
 
-	sc->nirqs = 2;
+	sc->nirqs = NIRQS;
 	riscv_register_root_pic(dev, sc->nirqs);
-
-	//csr_set(sstatus, SR_IE);
-	//csr_clear(sie, (1 << 5));
 
 	return (0);
 }
@@ -135,29 +126,29 @@ static void ric_dispatch(device_t dev, struct trapframe *frame)
 
 	sc = device_get_softc(dev);
 
-	active_irq = (frame->tf_scause & 0xf);
-	if (frame->tf_scause & (1 << 31)) {
-		//printf("ric_dispatch %d\n", active_irq);
+	if (frame->tf_scause & EXCP_INTR) {
+		active_irq = (frame->tf_scause & EXCP_MASK);
 		riscv_dispatch_intr(active_irq, frame);
-		//return;
 	}
-}
-
-static void
-ric_eoi(device_t dev, u_int irq)
-{
-	//struct riscv_ric_softc *sc = device_get_softc(dev);
-	//printf("%s\n", __func__);
-	//ric_c_write_4(sc, GICC_EOIR, irq);
 }
 
 void
 ric_mask_irq(device_t dev, u_int irq)
 {
-	//struct riscv_ric_softc *sc = device_get_softc(dev);
-	printf("ric_mask_irq\n");
-	//ric_d_write_4(sc, GICD_ICENABLER(irq >> 5), (1UL << (irq & 0x1F)));
-	//ric_c_write_4(sc, GICC_EOIR, irq);
+	struct riscv_ric_softc *sc;
+
+	sc = device_get_softc(dev);
+
+	switch (irq) {
+	case IRQ_TIMER:
+		csr_clear(sie, SIE_STIE);
+		break;
+	case IRQ_SOFTWARE:
+		csr_clear(sie, SIE_SSIE);
+		break;
+	default:
+		panic("Unknown irq %d\n", irq);
+	}
 }
 
 void
@@ -167,22 +158,16 @@ ric_unmask_irq(device_t dev, u_int irq)
 
 	sc = device_get_softc(dev);
 
-	//printf("ric_unmask_irq %d\n", irq);
-
 	switch (irq) {
 	case IRQ_TIMER:
 		csr_set(sie, SIE_STIE);
 		break;
 	case IRQ_SOFTWARE:
-		//csr_set(sie, SIE_SSIE);
+		csr_set(sie, SIE_SSIE);
 		break;
 	default:
 		panic("Unknown irq %d\n", irq);
 	}
-
-	//panic("%s: %d\n", __func__, irq);
-	//printf("%s: %d\n", __func__, irq);
-	//ric_d_write_4(sc, GICD_ISENABLER(irq >> 5), (1UL << (irq & 0x1F)));
 }
 
 static device_method_t riscv_ric_methods[] = {
@@ -192,7 +177,6 @@ static device_method_t riscv_ric_methods[] = {
 
 	/* pic_if */
 	DEVMETHOD(pic_dispatch,		ric_dispatch),
-	DEVMETHOD(pic_eoi,		ric_eoi),
 	DEVMETHOD(pic_mask,		ric_mask_irq),
 	DEVMETHOD(pic_unmask,		ric_unmask_irq),
 
