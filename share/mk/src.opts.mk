4b625b889c57b4a704718ddb8d759418408b1d4a
Ruslan Bukin <br@bsdpad.com>
2015-12-09 11:10:24 +0000

    Add comments for BROKEN options

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -235,7 +235,10 @@ __DEFAULT_YES_OPTIONS+=ELFCOPY_AS_OBJCOPY
 __DEFAULT_NO_OPTIONS+=ELFCOPY_AS_OBJCOPY
 .endif
 .if ${__T} == "riscv"
-BROKEN_OPTIONS+=PROFILE TESTS CXX RESCUE
+BROKEN_OPTIONS+=PROFILE # "sorry, unimplemented: profiler support for RISC-V"
+BROKEN_OPTIONS+=TESTS   # "undefined reference to `_Unwind_Resume'"
+BROKEN_OPTIONS+=CXX     # "contents.ms:38: macro error: KE without KS or KF"
+BROKEN_OPTIONS+=RESCUE  # "chio.lo: unknown executable format"
 .endif
 # LLVM lacks support for FreeBSD 64-bit atomic operations for ARMv4/ARMv5
 .if ${__T} == "arm" || ${__T} == "armeb"
