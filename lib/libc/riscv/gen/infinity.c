3dc7c0acdd3ce345acdd532271bfd5f2039ed52e
Ruslan Bukin <br@bsdpad.com>
2015-12-21 11:38:58 +0000

    Cleanup

--- lib/libc/riscv/gen/infinity.c
+++ lib/libc/riscv/gen/infinity.c
@@ -7,7 +7,7 @@ __FBSDID("$FreeBSD$");
 
 #include <math.h>
 
-/* bytes for +Infinity on aarch64 */
+/* bytes for +Infinity on riscv */
 const union __infinity_un __infinity = { { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f } };
 
 /* bytes for NaN */
