86a3439bdab5880f1a7dc06c1fd39ed2fa7b041e
Ruslan Bukin <br@bsdpad.com>
2016-01-21 16:23:37 +0000

    Better ordering

--- contrib/compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.h
+++ contrib/compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.h
@@ -19,9 +19,9 @@ namespace __sanitizer {
 
 static const u32 kStackTraceMax = 256;
 
-#if SANITIZER_LINUX && (defined(__aarch64__) || defined(__powerpc__) || \
-                        defined(__powerpc64__) || defined(__sparc__) || \
-                        defined(__mips__) || defined(__riscv__))
+#if SANITIZER_LINUX && (defined(__aarch64__) || defined(__mips__) || \
+                        defined(__powerpc__) || defined(__powerpc64__) || \
+                        defined(__riscv__) || defined(__sparc__))
 # define SANITIZER_CAN_FAST_UNWIND 0
 #elif SANITIZER_WINDOWS
 # define SANITIZER_CAN_FAST_UNWIND 0
