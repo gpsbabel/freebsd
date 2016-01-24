5451d06d75e6d7e7ae49f6310153a8499a256e2e
Ruslan Bukin <br@bsdpad.com>
2016-01-24 12:46:52 +0000

    __riscv__ -> __riscv64

--- contrib/elftoolchain/libelf/_libelf_config.h
+++ contrib/elftoolchain/libelf/_libelf_config.h
@@ -97,7 +97,7 @@
 #define	LIBELF_BYTEORDER	ELFDATA2MSB
 #define	LIBELF_CLASS		ELFCLASS32
 
-#elif	defined(__riscv__)
+#elif	defined(__riscv64)
 
 #define	LIBELF_ARCH		EM_RISCV
 #define	LIBELF_BYTEORDER	ELFDATA2LSB
