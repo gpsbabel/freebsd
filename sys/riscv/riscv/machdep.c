01232966987e5d26163840a0bdee370d2bc4567d
Ruslan Bukin <br@bsdpad.com>
2016-02-03 15:44:50 +0000

    Use gp for pcpu. Store it on stack

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -565,6 +565,7 @@ sendsig(sig_t catcher, ksiginfo_t *ksi, sigset_t *mask)
 static void
 init_proc0(vm_offset_t kstack)
 {
+
 	pcpup = &__pcpu[0];
 
 	proc_linkup0(&proc0, &thread0);
@@ -762,11 +763,7 @@ initriscv(struct riscv_bootparams *rvbp)
 	pcpu_init(pcpup, 0, sizeof(struct pcpu));
 
 	/* Set the pcpu pointer */
-#if 0
-	/* SMP TODO: try re-use gp for pcpu pointer */
-	__asm __volatile(
-	    "mv gp, %0" :: "r"(pcpup));
-#endif
+	__asm __volatile("mv gp, %0" :: "r"(pcpup));
 
 	PCPU_SET(curthread, &thread0);
 
