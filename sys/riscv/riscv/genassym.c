ba51fab4e0e9443085cd21803154eb17b992af14
Ruslan Bukin <br@bsdpad.com>
2016-02-23 17:33:48 +0000

    cleanup

--- sys/riscv/riscv/genassym.c
+++ sys/riscv/riscv/genassym.c
@@ -75,7 +75,6 @@ ASSYM(PCB_A, offsetof(struct pcb, pcb_a));
 ASSYM(SF_UC, offsetof(struct sigframe, sf_uc));
 
 ASSYM(PC_CURPCB, offsetof(struct pcpu, pc_curpcb));
-ASSYM(PC_CPUID, offsetof(struct pcpu, pc_cpuid));
 ASSYM(PC_SPTBR, offsetof(struct pcpu, pc_sptbr));
 ASSYM(PC_CURTHREAD, offsetof(struct pcpu, pc_curthread));
 
