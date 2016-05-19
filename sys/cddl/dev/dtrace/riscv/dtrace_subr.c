fdaca23c914c727899f8e87820960f9dc9621a54
Ruslan Bukin <br@bsdpad.com>
2016-05-19 17:03:41 +0000

    cleanup

--- sys/cddl/dev/dtrace/riscv/dtrace_subr.c
+++ sys/cddl/dev/dtrace/riscv/dtrace_subr.c
@@ -240,83 +240,27 @@ dtrace_invop_start(struct trapframe *frame)
 	int data, invop, reg, update_sp;
 	register_t arg1, arg2;
 	register_t *sp;
+	uint32_t imm;
 	InstFmt i;
 	int offs;
 	int tmp;
-	//int i;
 
 	invop = dtrace_invop(frame->tf_sepc, frame, frame->tf_sepc);
 
 	if (invop == RET_INSTR) {
-		printf("%s: %x\n", __func__, invop);
+		//printf("%s: %x\n", __func__, invop);
+		frame->tf_sepc = frame->tf_ra;
 		return (0);
 	}
 
-	i.word = invop;
-	uint32_t imm;
-
 	if ((invop & SD_RA_SP_MASK) == SD_RA_SP) {
+		i.word = invop;
 		imm = i.SType.imm0_4 | (i.SType.imm5_11 << 5);
 		sp = (register_t *)((uint8_t *)frame->tf_sp + imm);
-		printf("imm 0x%x, sp 0x%016lx, sp 0x%016lx\n",
-		    imm, frame->tf_sp, sp);
-
 		*sp = frame->tf_ra;
-
 		frame->tf_sepc += INSN_SIZE;
 		return (0);
 	}
-
-#if 0
-	tmp = (invop & LDP_STP_MASK);
-	if (tmp == STP_64 || tmp == LDP_64) {
-		sp = (register_t *)frame->tf_sp;
-		data = invop;
-		arg1 = (data >> ARG1_SHIFT) & ARG1_MASK;
-		arg2 = (data >> ARG2_SHIFT) & ARG2_MASK;
-
-		offs = (data >> OFFSET_SHIFT) & OFFSET_MASK;
-
-		switch (tmp) {
-		case STP_64:
-			if (offs >> (OFFSET_SIZE - 1))
-				sp -= (~offs & OFFSET_MASK) + 1;
-			else
-				sp += (offs);
-			*(sp + 0) = frame->tf_x[arg1];
-			*(sp + 1) = frame->tf_x[arg2];
-			break;
-		case LDP_64:
-			frame->tf_x[arg1] = *(sp + 0);
-			frame->tf_x[arg2] = *(sp + 1);
-			if (offs >> (OFFSET_SIZE - 1))
-				sp -= (~offs & OFFSET_MASK) + 1;
-			else
-				sp += (offs);
-			break;
-		default:
-			break;
-		}
-
-		/* Update the stack pointer and program counter to continue */
-		frame->tf_sp = (register_t)sp;
-		frame->tf_elr += INSN_SIZE;
-		return (0);
-	}
-
-	if ((invop & B_MASK) == B_INSTR) {
-		data = (invop & B_DATA_MASK);
-		/* The data is the number of 4-byte words to change the pc */
-		data *= 4;
-		frame->tf_elr += data;
-		return (0);
-	}
-
-	if (invop == RET_INSTR) {
-		frame->tf_elr = frame->tf_lr;
-		return (0);
-	}
-#endif
 
 	return (-1);
 }
