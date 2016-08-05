bb847c7d56a099128a81094263e158db477fb8d9
Ruslan Bukin <br@bsdpad.com>
2016-08-05 15:43:20 +0000

    cleanup

--- sys/riscv/riscv/riscv_console.c
+++ sys/riscv/riscv/riscv_console.c
@@ -305,7 +305,7 @@ rcons_probe(device_t dev)
 	if (!ofw_bus_status_okay(dev))
 		return (ENXIO);
 
-	if (!ofw_bus_is_compatible(dev, "riscv,rcons"))
+	if (!ofw_bus_is_compatible(dev, "riscv,console"))
 		return (ENXIO);
 
 	device_set_desc(dev, "RISC-V console");
